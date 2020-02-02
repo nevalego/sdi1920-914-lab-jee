be6b5fea7dbd094e986da325db8c551f341fb346
Nerea Valdés Egocheaga <nereavaldes@outlook.es>
2020-02-03 00:04:32 +0100

    SDI - 4 MVC, Modelo Vista Controlador

--- sdi1920-914-lab-jee/src/com/uniovi/sdi/ServletCarrito.java
+++ sdi1920-914-lab-jee/src/com/uniovi/sdi/ServletCarrito.java
@@ -1,8 +1,7 @@
 package com.uniovi.sdi;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.HashMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -33,44 +32,36 @@ public class ServletCarrito extends HttpServlet {
 			throws ServletException, IOException {
 
 		HttpSession session = request.getSession();
-		ConcurrentHashMap<String, Integer> carrito = (ConcurrentHashMap<String, Integer>) 
-				request.getSession().getAttribute("carrito");
-		// No hay carrito, creamos uno y lo insertamos en sesión
-		if (carrito == null) {
-			carrito = new ConcurrentHashMap <String, Integer>();
-			request.getSession().setAttribute("carrito", carrito);
-		}
+		HashMap<String, Integer> carrito = 
+				(HashMap<String, Integer>) request.getSession().getAttribute("carrito"); 													// }
+
 		String producto = request.getParameter("producto");
 		if (producto != null) {
 			insertarEnCarrito(carrito, producto);
-		}
-		response.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("<HTML>");
-		out.println("<HEAD><TITLE>Tienda SDI: carrito</TITLE></HEAD>");
-		out.println("<BODY>");
-		out.println(carritoEnHTML(carrito) + "<br>");
-		out.println("<a href=\"index.jsp\">Volver</a></BODY></HTML>");
-	}
-	
-	
+		} 
+		
+		// Retornar la vista con parámetro "carrito"
+		request.setAttribute("paresCarrito", carrito);
+		getServletContext().getRequestDispatcher("/vista-carrito.jsp").forward(request,
+		 response);
 
-	private String carritoEnHTML(ConcurrentHashMap<String, Integer> carrito) {
-		String carritoEnHTML = "";
-		for( String key: carrito.keySet())
-			carritoEnHTML+= "<p>["+key+"], "+carrito.get(key)+" unidades</p>";
-		return carritoEnHTML;
 	}
 
-	private void insertarEnCarrito(ConcurrentHashMap<String, Integer> carrito, String claveProducto) {
-		if( carrito.get(claveProducto) == null)
+//	private String carritoEnHTML(ConcurrentHashMap<String, Integer> carrito) {
+//		String carritoEnHTML = "";
+//		for (String key : carrito.keySet())
+//			carritoEnHTML += "<p>[" + key + "], " + carrito.get(key) + " unidades</p>";
+//		return carritoEnHTML;
+//	}
+
+	private void insertarEnCarrito(HashMap<String, Integer> carrito, String claveProducto) {
+		if (carrito.get(claveProducto) == null)
 			carrito.put(claveProducto, new Integer(1));
 		else {
-			int numeroArticulos = (Integer)carrito.get(claveProducto).intValue();
-			carrito.put(claveProducto, new Integer(numeroArticulos+1));
+			int numeroArticulos = (Integer) carrito.get(claveProducto).intValue();
+			carrito.put(claveProducto, new Integer(numeroArticulos + 1));
 		}
-		
+
 	}
 
 	/**
